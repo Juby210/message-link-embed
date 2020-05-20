edcf1b9009824948d9f2d55bf5d19d9a5616e141
Juby210 <juby210@vp.pl>
2020-05-21 01:18:00 +0200

    discord.com is a real thing, use MessageContent component

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ const LinkEmbed = require('./LinkEmbed')
 
 module.exports = class MessageLinksEmbed extends Plugin {
     async startPlugin() {
-        const Message = await getModule(m => m.default && m.default.displayName == 'Message')
-        inject('mlembed', Message, 'default', a => {
-            const [{ childrenMessageContent: { props: { content, message } } }] = a
-            if (!content || !message) return a
-            const match = message.content.match(/https?:\/\/((canary|ptb)\.)?discordapp\.com\/channels\/(\d{17,19}|@me)\/\d{17,19}\/\d{17,19}/g)
-            if (!match || content.find(c => c.type && c.type.name == 'LinkEmbed')) return a
-            match.forEach(link => {
-                content.push(React.createElement(LinkEmbed, { link }))
-            })
+        const MessageContent = await getModule(m => m.type && m.type.displayName == 'MessageContent')
+        inject('mlembed', MessageContent, 'type', (_, res) => {
+            if (!Array.isArray(res.props.children[0]) ||
+                res.props.children[0].find(c => c.type && c.type.name == 'LinkEmbed')) return res
+            res.props.children[0].filter(c =>
+                c.type && c.type.displayName == 'MaskedLink' &&
+                c.props.href.match(/https?:\/\/((canary|ptb)\.)?discord(app)?\.com\/channels\/(\d{17,19}|@me)\/\d{17,19}\/\d{17,19}/g)
+            ).forEach(c => res.props.children.push(React.createElement(LinkEmbed, { link: c.props.href })))
 
-            return a
-        }, true)
-        Message.default.displayName = 'Message'
+            return res
+        })
+        MessageContent.type.displayName = 'MessageContent'
     }
 
     pluginWillUnload() {
